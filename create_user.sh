4915b558add349b375f57dcbcf163f695c82c3b1
Andreas Wombacher <andreas.wombacher@aureliusenterprise.com>
2024-03-13 15:11:20 +0100

    added the option for a user to belong into two groups

--- create_user.sh
+++ create_user.sh
@@ -4,4 +4,7 @@
 /opt/jboss/keycloak/bin/kcadm.sh create users -r m4i -s username=$1 -s enabled=true
 /opt/jboss/keycloak/bin/kcadm.sh add-roles -r m4i --uusername $1 --rolename ATLAS_USER
 /opt/jboss/keycloak/bin/kcadm.sh add-roles -r m4i --uusername $1 --rolename $3
+if [[ $# -eq 4]] ; then
+    /opt/jboss/keycloak/bin/kcadm.sh add-roles -r m4i --uusername $1 --rolename $4
+fi
 /opt/jboss/keycloak/bin/kcadm.sh set-password -r m4i --username $1 --new-password $2
--- init_standard_users.sh
+++ init_standard_users.sh
@@ -6,6 +6,6 @@ while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' curl \
     sleep 5;
 done;
 sleep 5;
-/tmp/create_user.sh atlas ${USER_ATLAS_PASSWORD} ROLE_ADMIN;
+/tmp/create_user.sh atlas ${USER_ATLAS_PASSWORD} ROLE_ADMIN DATA_STEWARD;
 /tmp/create_user.sh steward ${USER_STEWARD_PASSWORD} DATA_STEWARD;
 /tmp/create_user.sh scientist ${USER_DATA_PASSWORD} DATA_SCIENTIST;      
