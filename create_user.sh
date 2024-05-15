4602994ec923022705fa3d9e5850adab2a32e292
matjgora <mateusz.gora@aureliusenterprise.com>
2024-05-15 12:20:24 +0200

    parametrize realm name

--- create_user.sh
+++ create_user.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 PORT=${KEYCLOAK_PORT:-8080}
+REALM_NAME=${KEYCLOAK_REALM_NAME:-"m4i"}
 /opt/jboss/keycloak/bin/kcadm.sh config credentials --server "http://localhost:${PORT}/auth" --realm master --user admin --password $KEYCLOAK_PASSWORD
-/opt/jboss/keycloak/bin/kcadm.sh create users -r m4i -s username=$1 -s enabled=true
-/opt/jboss/keycloak/bin/kcadm.sh add-roles -r m4i --uusername $1 --rolename ATLAS_USER
-/opt/jboss/keycloak/bin/kcadm.sh add-roles -r m4i --uusername $1 --rolename $3
+/opt/jboss/keycloak/bin/kcadm.sh create users -r "$REALM_NAME" -s username=$1 -s enabled=true
+/opt/jboss/keycloak/bin/kcadm.sh add-roles -r "$REALM_NAME" --uusername $1 --rolename ATLAS_USER
+/opt/jboss/keycloak/bin/kcadm.sh add-roles -r "$REALM_NAME" --uusername $1 --rolename $3
 if [ $# -eq 4 ]
 then
-    /opt/jboss/keycloak/bin/kcadm.sh add-roles -r m4i --uusername $1 --rolename $4
+    /opt/jboss/keycloak/bin/kcadm.sh add-roles -r "$REALM_NAME" --uusername $1 --rolename $4
 fi
-/opt/jboss/keycloak/bin/kcadm.sh set-password -r m4i --username $1 --new-password $2
+/opt/jboss/keycloak/bin/kcadm.sh set-password -r "$REALM_NAME" --username $1 --new-password $2
